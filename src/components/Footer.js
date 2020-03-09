81e68b7026cc46952dda4c529e6234a8e526f574
andresfelipedev <andresfelipedev@gmail.com>
2020-03-09 15:01:59 -0500

    Refactor the code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 
-const Footer = () => {
+const Footer = () => {    
     return (
         <Row className="footer">
-            <Col xs={12}>
-                <span className="text">
-                    Web site created by Andrés Felipe Pérez Rodríguez
-                </span>
-            </Col>
-            <Col xs={12}>
-                <span className="year">
-                    {new Date().getFullYear()}
-                </span>                
-            </Col>
+            <Col>
+                <a href={'https://andresfelipedev.github.io/resume'} target={'_blank'}>
+                    Web site created by Andrés Felipe Pérez Rodríguez | {new Date().getFullYear()}
+                </a>
+            </Col>            
         </Row>                
     );
 };
