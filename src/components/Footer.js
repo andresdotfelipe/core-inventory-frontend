778b1818a1ca5541c61cba3ddeeeac8f4529733b
andresfelipedev <andresfelipedev@gmail.com>
2021-07-09 16:49:02 -0500

    Update footer description

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <Row className="footer">
             <Col>
                 <a href={'https://andresfelipedev.github.io/portfolio'} target={'_blank'} rel={'noopener noreferrer'}>
-                    Web site created by Andrés Felipe Pérez Rodríguez
+                    Web application created by Andrés Felipe Pérez Rodríguez
                 </a>
             </Col>            
         </Row>                
