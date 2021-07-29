2e901a1673983a5c2862127f56208bf596db39ad
andresfelipedev <andresfelipedev@gmail.com>
2021-07-29 12:39:28 -0500

    feat: close navbar when user clicks outside of it and when  nav links are clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,31 @@ import { logOut, removeSession } from '../actions/users';
 import { Button, Col, Nav, Navbar, Row } from 'react-bootstrap';
 import Logo from '../assets/images/core-inventory-logo.svg';
 
-class Header extends PureComponent {    
+class Header extends PureComponent {   
+    
+    constructor(props) {  
+        super(props);
+        this.state = {
+            expandedNavbar: false
+        };
+        this.Navbar = React.createRef();
+    }
+
+    componentDidUpdate() {
+        window.addEventListener('mousedown', this.handleClickOutsideNavbar);
+        window.addEventListener('scroll', this.handleClickOutsideNavbar);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('mousedown', this.handleClickOutsideNavbar);
+        window.removeEventListener('scroll', this.handleClickOutsideNavbar);
+    }
+
+    // Closes Navbar if user clicks outside it.
+    handleClickOutsideNavbar = e => {
+        if ((e.clientX) >= e.target.clientWidth) return;
+        if (!this.Navbar.current.contains(e.target)) this.setState({ expandedNavbar: false });
+    };
 
     handleLogOut = () => {
         const { removeSession, logOut } = this.props;
@@ -21,16 +45,22 @@ class Header extends PureComponent {
                     {
                         session ?
                         <React.Fragment>
-                            <Navbar expand="lg" fixed="top">
+                            <Navbar 
+                                ref={this.Navbar} 
+                                expand="lg" 
+                                expanded={this.state.expandedNavbar}
+                                onToggle={() => this.setState({ expandedNavbar: !this.state.expandedNavbar })} 
+                                fixed="top" 
+                                collapseOnSelect>
                                 <Link to="/inventory" className="brand">
                                     <img src={Logo} alt="Core Inventory" width="175px" />
                                 </Link>
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
                                     <Nav className="mr-auto">
-                                        <Link className="nav-link" to="/inventory">View inventory</Link>
-                                        <Link className="nav-link" to="/categories">Categories</Link>
-                                        <Link className="nav-link" to="/create-item">Create item</Link>                    
+                                        <Nav.Link as={Link} to="/inventory" eventKey="0">View inventory</Nav.Link>
+                                        <Nav.Link as={Link} to="/categories" eventKey="1">Categories</Nav.Link>
+                                        <Nav.Link as={Link} to="/create-item" eventKey="2">Create item</Nav.Link>                    
                                     </Nav>                    
                                     <Button 
                                         variant="outline-primary" 
