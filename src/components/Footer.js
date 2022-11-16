2626cd83f10d0c256cc26beefd841703b55ffa44
andresdotfelipe <andresdotfelipecs@outlook.com>
2022-11-16 18:38:46 -0500

    Update portfolio link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import React from "react";
+import { Col, Row } from "react-bootstrap";
 
-const Footer = () => {    
-    return (
-        <Row className="footer">
-            <Col>
-                <a href={'https://andresfelipedev.github.io/resume/'} target={'_blank'} rel={'noopener noreferrer'}>
-                    <span>Created by Andrés Felipe Pérez Rodríguez</span>
-                    <i className="fas fa-external-link-alt"></i>
-                </a>                
-            </Col>            
-        </Row>                
-    );
+const Footer = () => {
+  return (
+    <Row className="footer">
+      <Col>
+        <a
+          href={"https://andresdotfelipe.github.io/resume/"}
+          target={"_blank"}
+          rel={"noopener noreferrer"}
+        >
+          <span>Created by Andrés Felipe Pérez Rodríguez</span>
+          <i className="fas fa-external-link-alt"></i>
+        </a>
+      </Col>
+    </Row>
+  );
 };
 
 export default Footer;
